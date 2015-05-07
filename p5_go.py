745c9426f67f0fc6a23fe5b28c710344f2bd5798
Mark Biundo <mdbiundo@ucsc.edu>
2015-05-07 13:29:45 -0700

    A* fixed and added test case

--- p5_go.py
+++ p5_go.py
@@ -27,6 +27,7 @@ print Crafting['Recipes']['craft stone_pickaxe at bench']
 #	'Time': 1
 # }
 
+
 def make_checker(rule):
 	# this code runs once
 	# do something with rule['Consumes'] and rule['Requires']
@@ -56,11 +57,13 @@ for name, rule in Crafting['Recipes'].items():
 	effector = make_effector(rule)
 	recipe = Recipe(name, checker, effector, rule['Time'])
 	all_recipes.append(recipe)
-	
+
+from heapq import heappush, heappop	
 def search(graph, initial, is_goal, limit, heuristic):
 	dist = {}
 	dist[initial] = 0
 	prev = {}
+	prev[initial] = None
 	queue = []
 	heappush(queue, (dist[initial], initial))
 	finished = False
@@ -70,6 +73,8 @@ def search(graph, initial, is_goal, limit, heuristic):
 		counter += 1
 		bdist, state = heappop(queue)
 		
+		#print str(bdist) + " " + str(state)
+		
 		if is_goal(state):
 			finished = True
 			break
@@ -78,9 +83,11 @@ def search(graph, initial, is_goal, limit, heuristic):
 		
 		for n in neighbors:
 			alt = bdist + n[2] + heuristic(n[1])
+			#print str(n) + " " + str(alt)
 			if n[1] not in dist or alt < dist[n[1]]:
 				dist[n[1]] = alt
 				prev[n[1]] = state
+				#print "Prev of " + str(n[1]) + " = " + str(prev[n[1]])
 				heappush(queue, (dist[n[1]], n[1]))
 
 				
@@ -88,8 +95,9 @@ def search(graph, initial, is_goal, limit, heuristic):
 		plan = []
 		total_cost = bdist
 		while state:
+			#print state
 			plan.append(state)
-			state = prev[state] 
+			state = prev[state]
 		plan.reverse()
 		
 	else:
@@ -98,3 +106,20 @@ def search(graph, initial, is_goal, limit, heuristic):
 		total_cost = 0
 
 	return total_cost, plan
+	
+print "Testing A* search"
+t_initial = 'a'
+t_limit = 20
+edges = {'a': {'b': 1, 'c': 10}, 'b':{'c':1}}
+
+def t_graph(state):
+	for next_state, cost in edges[state].items():
+		yield ((state, next_state), next_state, cost)
+		
+def t_is_goal(state):
+	return state == 'c'
+	
+def t_heuristic(state):
+	return 0
+
+print search(t_graph, t_initial, t_is_goal, t_limit, t_heuristic)
